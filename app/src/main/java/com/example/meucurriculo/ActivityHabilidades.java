18bb58a09175a44e81c5aca117792f9c0fd011d3
gustavvu <gustavvupinto@gmail.com>
2023-05-23 13:33:02 -0300

    Tela de Habilidades

--- app/src/main/java/com/example/meucurriculo/ActivityHabilidades.java
+++ app/src/main/java/com/example/meucurriculo/ActivityHabilidades.java
@@ -9,19 +9,11 @@ import android.widget.ImageButton;
 
 public class ActivityHabilidades extends AppCompatActivity {
 
-    ImageButton IBBack;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_habilidades);
 
-        IBBack=findViewById(R.id.imageButtonBackId);
-
-        IBBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
     }
 }

12515910eb91f45f4e72a40b330eb28452576cdd
gustavvu <gustavvupinto@gmail.com>
2023-05-24 19:12:34 -0300

    Habilidades 2

--- app/src/main/java/com/example/meucurriculo/MainActivity.java
+++ app/src/main/java/com/example/meucurriculo/MainActivity.java
@@ -12,7 +12,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     Button botaoHabilidades, botaoProjetos, botaoEdu;
-    TextView textViewEmail, textViewNumero;
+    TextView textViewEmail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,21 +22,11 @@ public class MainActivity extends AppCompatActivity {
         botaoHabilidades=findViewById(R.id.buttonHabilidadesId);
         botaoEdu=findViewById(R.id.buttonEduId);
         textViewEmail=findViewById(R.id.textViewEmailId);
-        textViewNumero=findViewById(R.id.textViewNumeroId);
 
         textViewEmail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Uri uri = Uri.parse("mailto:gustavvupinto@gmail.com"); // missing 'http://' will cause crashed
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
-            }
-        });
-
-        textViewNumero.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Uri uri = Uri.parse("https://api.whatsapp.com/send?phone=559881476139&text=Olá,%20Vi%20o%20seu%20curriculo%20e%20o%20achei%20interesante"); // missing 'http://' will cause crashed
                 Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                 startActivity(intent);
             }

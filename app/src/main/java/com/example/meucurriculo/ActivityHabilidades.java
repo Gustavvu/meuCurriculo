6f9b1598c123473be659c7e22fbe9237e15a0813
gustavvu <gustavvupinto@gmail.com>
2023-05-24 13:54:29 -0300

    Educação e idiomas

--- app/src/main/java/com/example/meucurriculo/ActivityHabilidades.java
+++ app/src/main/java/com/example/meucurriculo/ActivityHabilidades.java
@@ -6,14 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class ActivityHabilidades extends AppCompatActivity {
 
+    TextView textVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_habilidades);
+            textVoltar=findViewById(R.id.textView10);
 
+            textVoltar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish();
+                }
+            });
     }
 }
